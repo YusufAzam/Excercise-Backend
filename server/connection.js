37f91a93ea3aab6b7391cf8dc4efb76171711805
Yusuf Azam <yusufazam@lostcoding.com>
2023-06-16 20:56:46 +0100

    setting up connection for mgdb

---
+++ server/connection.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+
+export const connectToMongoDb = () => {
+    const uri = process.env.ATLAS_URI;
+    mongoose.connect(uri);
+}
---
+++ server/server.js
@@ -0,0 +1,19 @@
+import  express from 'express';
+import  cors from 'cors';
+import { connectToMongoDb } from './connection.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+connectToMongoDb();
+
+app.listen(port, ()=> {
+    console.log(`running on port ${port}`);
+})
