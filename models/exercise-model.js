5741bcb16cbe673ae3186fa5a7a65987329f9305
Yusuf Azam <yusufazam@lostcoding.com>
2023-06-16 21:48:29 +0100

    make the project look pretty

--- models/exercise-model.js
+++ models/exercise-model.js
@@ -4,6 +4,23 @@ const Schema = mongoose.Schema;
 
 const exceriseSchema = new Schema({
     username: {
-        
-    }
-})
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    }, 
+}, {
+    timestamps: true
+});
+
+export const Excercise = mongoose.model('Excerise', exceriseSchema);
