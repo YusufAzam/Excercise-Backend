4037f6c1e8c27377e78c55fb5a86c64f4e6bf3a9
Yusuf Azam <yusufazam@lostcoding.com>
2023-06-18 15:03:08 +0100

    all endpoints setup

--- src/routes/excercise.js
+++ src/routes/excercise.js
@@ -10,6 +10,7 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
+  //needs validation
  const {username, description, duration, date} =  req.body;
 
  const newExercise = new Exercise({
@@ -22,4 +23,33 @@ router.route('/add').post((req, res) => {
   newExercise.save()
   .then(() => res.json('Exercise added!'))
   .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').put((req, res) => {
+  //needs validation
+  const  {username, description , duration, date} = req.body;
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = username;
+      exercise.description = description;
+      exercise.duration = Number(duration);
+      exercise.date = Date.parse(date);
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
